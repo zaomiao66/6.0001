20997cb8935fa2a06c747c733f02739de4eea890
zhouzihao <zhouzihao@xiaoniangao.com>
2020-06-08 17:46:54 +0800

    ps0

---
+++ ps0.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 17:22:47 2020
+
+@author: zao
+"""
+import numpy
+
+base_number = int(input('Please input base number: '))
+
+power_number = int(input('Please input power number: '))
+
+print(base_number ** power_number)
+
+print(numpy.log2(base_number))
